ea0d44b00a0aad74ff38f62befef9a6242c9b5b1
Marcelo  Montenegro <montenegromf@gmail.com>
2022-02-09 19:14:14 +0000

    Add key functions on javascript code and test it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("I am linked properly!");
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log("I am workin!");
+}
+
+function setNextQuestion() {
+
+}
+//function showQuestion(question) {}
+
+
+//function resetState() {}
+
+
+function selectAnswer {
+
+}
