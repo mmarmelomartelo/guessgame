471fd686c320b1b67327c2286f966e52c2e67d6f
Marcelo  Montenegro <montenegromf@gmail.com>
2022-02-09 23:25:31 +0000

    Implement the functions select answer and setStatus

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,87 +28,116 @@ function startGame() {
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question;
     question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('button')
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('button');
         if (answer.correct) {
-            button.dataset.correct = answer.correct
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
 }
 
 
-function resetState() {
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("incorrect");
     }
+}
 
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("incorrect");
+}
 
-
-
-    function selectAnswer(e) {
-
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach((button) => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide");
+    } else {
+        startButton.innerText = "Restart";
+        startButton.classList.remove("hide");
     }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+}
 
 
-    const questions = [{
-            question: 'What is the capital of Sweden?',
-            answers: [{
-                    text: 'Stockholm',
-                    correct: true
-                },
-                {
-                    text: 'Lisbon',
-                    correct: false
-                }
-            ]
-        },
-        {
-            question: 'What is the capital of Finland?',
-            answers: [{
-                    text: 'Helsink',
-                    correct: true
-                },
-                {
-                    text: 'Stockholm',
-                    correct: false
-                }
-            ]
-        },
-        {
-            question: 'What is the capital of Norway?',
-            answers: [{
-                    text: 'Oslo',
-                    correct: true
-                },
-                {
-                    text: 'Helsink',
-                    correct: false
-                }
-            ]
-        },
+const questions = [{
+        question: 'What is the capital of Sweden?',
+        answers: [{
+                text: 'Stockholm',
+                correct: true
+            },
+            {
+                text: 'Lisbon',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What is the capital of Finland?',
+        answers: [{
+                text: 'Helsink',
+                correct: true
+            },
+            {
+                text: 'Stockholm',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What is the capital of Norway?',
+        answers: [{
+                text: 'Oslo',
+                correct: true
+            },
+            {
+                text: 'Helsink',
+                correct: false
+            }
+        ]
+    },
 
-        {
-            question: 'What is the capital of Denmark?',
-            answers: [{
-                    text: 'Copenhagen',
-                    correct: true
-                },
-                {
-                    text: 'Oslo',
-                    correct: false
-                }
-            ]
-        }
-    ];
+    {
+        question: 'What is the capital of Denmark?',
+        answers: [{
+                text: 'Copenhagen',
+                correct: true
+            },
+            {
+                text: 'Oslo',
+                correct: false
+            }
+        ]
+    }
+];
